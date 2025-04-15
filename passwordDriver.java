9f2a002dc2bbd90b19996baba9ad455f6c07b221
Ryan R Vargas <ryanr99@live.com>
2025-04-15 15:32:25 -0400

    Functions and test cases implemented

--- passwordDriver.java
+++ passwordDriver.java
@@ -1,2 +1,13 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
 public class passwordDriver {
+    public static void main(String[] args) {
+        passwordGenerator p = new passwordGenerator(12);
+        p.generateP();
+        p.generateP();
+        p.generateP();
+        p.generateP();
+
+        p.getSavedP();
+    }
 }
--- passwordGenerator.java
+++ passwordGenerator.java
@@ -1,21 +1,32 @@
+import java.util.ArrayList;
+
 public class passwordGenerator {
     private String password;
     private int length;
     private final String c = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-={}[]|:;<>,.?/";
+    ArrayList<String> savedP = new ArrayList<String>();
+
 
     public passwordGenerator(int length){
         this.length = length;
-        this.password = null;
+        this.password = "";
     }
 
-    public String getPassword(){
-        return password;
+    public void getSavedP(){
+        int index = 1;
+        for (String p : savedP){
+            System.out.printf("%d:Password:%s\n",index, p);
+            index += 1;
+        }
     }
 
     public void generateP(){
         for (int i = 0; i < length; i++){
-            password += c.charAt((int)(Math.random()*c.length()));
+            this.password += c.charAt((int)(Math.random()*c.length()));
         }
+        savedP.add(this.password);
+        this.password = "";
     }
+
 
 }
