66897dec580dde1b4b6271d1e8a3a7bea3004c28
Ryan R Vargas <ryanr99@live.com>
2025-04-23 12:41:05 -0400

    Adjustments made from merge

--- src/main/java/com/example/passwordgenerator/PasswordApplication.java
+++ src/main/java/com/example/passwordgenerator/PasswordApplication.java
@@ -6,32 +6,17 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-/**
- * The HelloApplication class is the main entry point for the JavaFX application.
- * It loads the FXML file, sets the stage, and launches the GUI.
- */
+
 public class PasswordApplication extends Application {
-    /**
-     * Called automatically when the JavaFX application is launched.
-     * It sets up the primary stage and loads the FXML layout.
-     * Polymorphism Used
-     * @param stage The primary stage for this application
-     * @throws IOException If the FXML file cannot be loaded
-     */
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(PasswordApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 320, 320);
+        stage.setTitle("Password Generator");
         stage.setScene(scene);
         stage.show();
     }
 
-    /**
-     * The main method, which launches the JavaFX application.
-     *
-     * @param args The command-line arguments (not used)
-     */
     public static void main(String[] args) {
         launch();
     }
